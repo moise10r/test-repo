2bacc1fe95daebf2f0672565c506ea6d5ff4d6c5
moise10r <moiserushanika2006@gmail.com>
2021-07-20 10:28:23 +0200

    Remove the src folder and the dist.

--- src/data.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable import/no-cycle */
-import { addTask } from './controllers';
-
-const data = addTask();
-export function getTask() {
-  return data.map((task) => task);
-}
